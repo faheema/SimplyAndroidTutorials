4a86640f23abd2b466c4ccd5ce208216b13f5d8c
faheem.u <faheem.fu@gmail.com>
2015-10-20 17:24:23 +0530

    Layouts working example

---
+++ SimplyLayoutsDemo/app/src/main/java/fam/sa/simplylayoutsdemo/TableLayoutDemo.java
@@ -0,0 +1,13 @@
+package fam.sa.simplylayoutsdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TableLayoutDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout_demo);
+    }
+}
