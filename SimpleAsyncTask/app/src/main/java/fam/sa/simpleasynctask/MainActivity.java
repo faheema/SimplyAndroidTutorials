a81637ce22979a38f4efc80dece74d3559a1e864
faheem.u <faheem.fu@gmail.com>
2015-10-29 18:28:27 +0530

    Async task

--- SimpleAsyncTask/app/src/main/java/fam/sa/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/fam/sa/simpleasynctask/MainActivity.java
@@ -2,8 +2,15 @@ package fam.sa.simpleasynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivShowImage;
+    Button btn_dwnlodImage;
+
+    private  String IMG_URL=
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
