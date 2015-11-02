c504f61faf00180bf6c3172bc64ae6a9ea17d8cf
faheem.u <faheem.fu@gmail.com>
2015-11-02 19:01:24 +0530

    Google MAPS demo

---
+++ GoogleMapsDemo/app/src/main/java/fam/sa/googlemapsdemo/MapsActivity.java
@@ -0,0 +1,93 @@
+package fam.sa.googlemapsdemo;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    public void onSearchClick(View view)
+    {
+        List<Address> addrList=null;
+                EditText text= (EditText) findViewById(R.id.etAddress);
+        String locationStr = text.getText().toString();
+        try {
+        if(locationStr!=null && !locationStr.equals(""))
+        {
+            Geocoder geocoder = new Geocoder(this);
+               addrList=  geocoder.getFromLocationName(locationStr.trim(),1);
+        }
+        Address address= addrList.get(0);
+        LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (NullPointerException np)
+        { np.printStackTrace();}
+
+    }
+    public void onChangeType(View view)
+    {
+        ToggleButton btn= (ToggleButton)view;
+        if(btn.isChecked())
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+/*
+    public void onZoom(View view)
+    {
+        if(view.getId() == R.id.Bzoomin)
+        {
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        }
+        if(view.getId() == R.id.Bzoomout)
+        {
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+        }
+    }*/
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker in Sydney"));
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
